1d6e91711c672b6010d56491a24011bb88bf91a4
FPEPOSHI <festimpeposhi95@gmail.com>
2016-01-08 12:15:43 +0100

    hgdhgfd

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -131,17 +131,17 @@
                             </p>
                         </li>
                         <!-- Menu Body -->
-                        <li class="user-body">
-                            <div class="col-xs-4 text-center">
-                                <a href="#">Profile</a>
-                            </div>
-                            <div class="col-xs-4 text-center">
-                                <a href="#">Sales</a>
-                            </div>
-                            <div class="col-xs-4 text-center">
-                                <a href="#">Friends</a>
-                            </div>
-                        </li>
+                        {{--<li class="user-body">--}}
+                            {{--<div class="col-xs-4 text-center">--}}
+                                {{--<a href="#">Profile</a>--}}
+                            {{--</div>--}}
+                            {{--<div class="col-xs-4 text-center">--}}
+                                {{--<a href="#">Sales</a>--}}
+                            {{--</div>--}}
+                            {{--<div class="col-xs-4 text-center">--}}
+                                {{--<a href="#">Friends</a>--}}
+                            {{--</div>--}}
+                        {{--</li>--}}
                         <!-- Menu Footer-->
                         <li class="user-footer">
                             <div class="pull-left">
