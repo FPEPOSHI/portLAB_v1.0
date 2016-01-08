1788e486ae1544b48932b70357cc15970e800e58
Siana Deshati <siana.deshati@gmail.com>
2016-01-08 12:01:08 +0100

    edit

---
+++ resources/views/settings.blade.php
@@ -0,0 +1,24 @@
+<div class="row" id="settings">
+    <div class="col-md-8">
+        <div class="container"><h2>Information </h2></div>
+        <div id="exTab3" class="container">
+            <ul  class="nav nav-pills">
+                <li class="active">
+                    <a  href="#1b" data-toggle="tab">Ndrysho informacione</a>
+                </li>
+                <li><a href="#2b" data-toggle="tab">Ndrysho fjalekalim</a>
+                </li>
+            </ul>
+            <div class="modal-header">
+                <button type="button" class="close"
+                        data-dismiss="modal">
+                    <span aria-hidden="true">&times;</span>
+                    <span class="sr-only">Close</span>
+                </button>
+                <h4 class="modal-title" id="edit">
+                    Te dhena
+                </h4>
+            </div>
+        </div>
+    </div>
+</div>
