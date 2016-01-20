6c75521792de1664e06920d21376a9ce9ea46227
Siana Deshati <siana.deshati@gmail.com>
2016-01-20 10:46:50 +0100

    edit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -252,9 +252,9 @@ class HomeController extends Controller
             $t .= '<div class="col-sm-6">
                                 <p id="l-p-i'. $pro->project_id.' ">' . $pro->likes . '</p>';
             if ($pro->l_user)
-                $t .= ' <div id="like-p" '.onclick="like({!! $pro->project_id !!})".'  data-id=" ' . $pro->project_id . ' "><i id="  like-p-' . $pro->project_id . '"  class="fa fa-thumbs-o-up">Liked</i></div>';
+                $t .= ' <div id="like-p" onclick="like('.$pro->project_id .')"  data-id=" ' . $pro->project_id . ' "><i id="  like-p-' . $pro->project_id . '"  class="fa fa-thumbs-o-up">Liked</i></div>';
             else
-                $t .= ' <div id="like-p" '.onclick="like({!! $pro->project_id !!})".' data-id=" ' . $pro->project_id . '"><i id="like-p- ' . $pro->project_id . ' "  class="fa fa-thumbs-o-up">Like</i></div>
+                $t .= ' <div id="like-p" onclick="like('.$pro->project_id .')" data-id=" ' . $pro->project_id . '"><i id="like-p- ' . $pro->project_id . ' "  class="fa fa-thumbs-o-up">Like</i></div>
 
                             </div>
                             </div>
