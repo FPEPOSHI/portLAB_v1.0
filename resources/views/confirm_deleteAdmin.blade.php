22041c281ccb09ff522ecde0e795350ed7eb4ba7
Siana Deshati <siana.deshati@gmail.com>
2016-01-23 18:36:11 +0100

    edit

---
+++ resources/views/confirm_deleteAdmin.blade.php
@@ -0,0 +1,34 @@
+
+<!-- Modal -->
+<div class="modal fade" id="admin_del" tabindex="-1" role="dialog"
+     aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <!-- Modal Header -->
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">
+                        <span aria-hidden="true">&times;</span>
+                        <span class="sr-only">Close</span>
+                    </button>
+                    <h4 class="modal-title" id="myModalLabel"> Delete Project </h4>
+                </div>
+
+                <!-- Modal Body -->
+                <div class="modal-body">
+                    <p>Would you like to delete this project ?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" data-dismiss="modal" class="btn">Cancel</button>
+                    <a href="{!! URL::route("admindelete") !!}" class="btn btn-danger">OK</a>
+                </div>
+                <input type="hidden" value="" id="p-id-d-c"/>
+
+                <!-- Modal Footer -->
+
+            </div>
+        </div>
+
+    </div>
+
+
+
